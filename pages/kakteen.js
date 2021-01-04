f4ca008ea8070da9849e997f2f2ca09add540fe1
sara04z <76944455+sara04z@users.noreply.github.com>
2021-01-04 11:42:12 +0100

    rounded images

--- pages/kakteen.js
+++ pages/kakteen.js
@@ -5,8 +5,9 @@ export default function KakteenPage() {
   return (
     <Layout>
       <PageHeading title="Kakteen"></PageHeading>
-      <span classNames="p-2 m-2 text-gray-600">Hallo Welt</span>
+      <span className="p-2 m-2 text-gray-600">Hallo Welt</span>
       <img
+        className="rounded-lg"
         style={{ width: "320px" }}
         src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fjooinn.com%2Fimages%2Fnew-york-17.jpg&f=1&nofb=1"
       ></img>
